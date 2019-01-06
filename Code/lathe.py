519452b1fa8487d4aa70cf5a18526296bd24c450
Charles Jacobs <cjacobs@microsoft.com>
2019-01-06 15:10:37 -0800

    nonconvex carve seems to be working

--- Code/lathe.py
+++ Code/lathe.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 # Stepper motor control with A4988 drivers
 
 import fractions
@@ -16,9 +18,6 @@ STEP_X = 23
 # X: left (-) / right (+)
 # Y: forward (-) / back (+)
 
-    
-
-
 class lathe(object):
     def __init__(self, steps_per_second):
         self.reset()
@@ -35,25 +34,92 @@ class lathe(object):
         self.curr_x = 0
         self.curr_y = 0
 
-    def carve_convex(self, contour, l, r, start_y, min_y):
-        y = start_y
-        self.moveto(l, y, self.steps_per_second)
+    def carve_convex_contour(self, contour, start_x, end_x, start_y, min_y):
+        initial_l = math.floor(start_x) # initial lefthand end
+        l = math.floor(start_x)
+        r = math.ceil(end_x)
+        print('carve: {}, {}, {}, {}'.format(l, r, start_y, min_y))
+        y = math.ceil(start_y)
+        self.moveto(l, y)
         while l < r and y > min_y:
+            self.moveto(l, y)
+
             # update l
-            while contour(l) == y and l < r:
+            while math.ceil(contour(l)) >= y and l < r:
                 l += 1
-                self.moveto(l, y, self.steps_per_second)
-            y += 1
-            
-            x = l
-            while contour(x) < y and x < r:
-                self.moveto(x, y, self.steps_per_second)
+                self.moveto(l, y)
+
+            # advance y
+            y -= 1
+            self.moveto(l, y)
+            print('y: {} = {}\t'.format(y, math.ceil(contour(l))), end='')
+            print(' '*(l-initial_l), end='')
+
+            # carve
+            x = l+1
+            while math.ceil(contour(x)) <= y and x < r:
+                self.moveto(x, y)
+                print('.', end='')
                 x += 1
+
+            print(' '*(r-x), end='')
+            print('\t = {}'.format(math.ceil(contour(x-1))))
+
+    def carve_contour(self, contour, start_x, end_x, start_y, min_y):
+        extra_segments = []
+        initial_l = math.floor(start_x) # initial lefthand end
+        initial_r = math.ceil(end_x)
+        
+        l = math.floor(start_x)
+        r = math.ceil(end_x)
+        print('carve: {}, {}, {}, {}'.format(l, r, start_y, min_y))
+        y = math.ceil(start_y)
+        
+        self.moveto(self.curr_x, y);
+        self.moveto(l, y)
+
+        while l < r and y > min_y:
+            print('{} -- {}'.format(l, r))
+            self.moveto(l, y)
+
+            # update l
+            while math.ceil(contour(l)) >= y and l <= r:
+                l += 1
+                self.moveto(l, y)
+
+            if l == r:
+                break
+
+            # advance y
+            y -= 1
+            self.moveto(l, y)
+            # print('y: {} = {}\t'.format(y, math.ceil(contour(l))), end='')
+            print(' '*(l-initial_l), end='', flush=True)
+
+            # carve
+            x = l
+            while math.ceil(contour(x)) <= y and x < r:
+                self.moveto(x, y)
+                print('.', end='', flush=True)
+                x += 1
+
+            print(' '*(initial_r-x), end='', flush=True)
+            print(' span: ({}, {}), {}'.format(l, x-1, y))
+            # print('\t = {}'.format(math.ceil(contour(x-1))))
+            old_r = r
+            r = x
+
+            if x+1 < old_r:
+                extra_segments += [(x+1, old_r, y, min_y)]
+        
+        # Now recursively do the rest
+        for args in extra_segments:
+            self.carve_contour(contour, *args)
 
     def moveto(self, x, y):
         dx = x - self.curr_x
         dy = y - self.curr_y
-        self.move(dx, dy, self.steps_per_second)
+        self.move(dx, dy)
 
     def move(self, dx, dy):
         if dx == 0 and dy == 0:
@@ -65,7 +131,6 @@ class lathe(object):
         dist = math.sqrt(dx*dx + dy*dy)
         gcd = fractions.gcd(max(1, dx), max(1, dy))
         iter = max(1, dx) * max(1, dy) // gcd
-        wait_time = (dist / iter) / self.steps_per_second
         inc_x = dx // gcd
         inc_y = dy // gcd
         step_count = 1
@@ -83,7 +148,7 @@ class lathe(object):
                     iy += iter
                     self.step_y(dir_y * 1)
                 
-                time.sleep(wait_time)
+                time.sleep(self.get_wait_time() * (dist/iter))
 
     def step_x(self, count):
         self.set_x_dir(1 if count > 0 else 0)
@@ -101,6 +166,12 @@ class lathe(object):
     def set_y_dir(self, d):
         gpio.output(DIR_Y, d)
     
+    def get_wait_time(self):
+        if self.steps_per_second == 0:
+            return 0
+        
+        return 1.0 / self.steps_per_second
+
     def pulse(self, pin, count):
         for i in range(count):
             gpio.output(pin, gpio.HIGH)
@@ -114,11 +185,17 @@ if __name__ == '__main__':
     xs = coords[0::2]
     ys = coords[1::2]
     coords = zip(xs, ys)
-    l = lathe(steps_per_second=150.0)
+    l = lathe(steps_per_second=0)
     l.setup()
 
-    for x, y in coords:
-        print(x, y)
-        l.moveto(x, y)
+    # for x, y in coords:
+    #     print(x, y)
+    #     l.moveto(x, y)
+
+    halfwidth = 50
+    contour = lambda x: x*x / halfwidth
+    l.carve_contour(contour, -halfwidth, halfwidth, halfwidth, -halfwidth)
+    # contour = lambda x: (x*x*x + 50*x*x) / (halfwidth*halfwidth)
+    # l.carve_contour(contour, -75, 10, 15, -5)
 
     gpio.cleanup()
