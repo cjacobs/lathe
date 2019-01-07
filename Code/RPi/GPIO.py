62f4f72f5608c8ed5082fe350dc9bd9c6f3c00e9
Charles Jacobs <cjacobs@microsoft.com>
2019-01-06 17:21:23 -0800

    Added null RPi.GPIO implementation for testing

---
+++ Code/RPi/GPIO.py
@@ -0,0 +1,20 @@
+# null implementation of RPi.GPIO module
+
+BCM = 0
+
+OUT = 1
+
+LOW = 0
+HIGH = 1
+
+def setmode(mode):
+    pass
+
+def setup(pin, mode):
+    pass
+
+def output(pin, value):
+    pass
+
+def cleanup():
+    pass
