4978265b607bced846b66a9dad04ab8625e5f58b
Charles Jacobs <cjacobs@microsoft.com>
2019-07-22 22:22:31 -0700

    added knobs file

---
+++ Code/knobs.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+# Read control knobs
+
+import argparse
+import sys
+import time
+
+# TODO: if testing, import RPi_fake.GPIO...
+import RPi.GPIO as gpio # https://pypi.python.org/pypi/RPi.GPIO more info
+
+# GPIO numbers, not pin numbers
+STEP_L = 5 # brown
+DIR_L = 6 # red
+SWITCH_L = 13 # gray
+
+STEP_R = 12 # blue
+DIR_R = 16 # purple
+SWITCH_R = 26 # orange
+
+
+def init():
+    gpio.setmode(gpio.BCM)
+    gpio.setup(STEP_L, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(DIR_L, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(SWITCH_L, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(STEP_R, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(DIR_R, gpio.IN, pull_up_down=gpio.PUD_UP)
+    gpio.setup(SWITCH_R, gpio.IN, pull_up_down=gpio.PUD_UP)
+
+def loop():
+    while True:
+        button_state = GPIO.input(STEP_L)
+        if not button_state:
+            print("X")
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--speed', type=float, help='steps per second', default=200)
+    args = parser.parse_args()
+    print(args)
+    init()
+    loop()
