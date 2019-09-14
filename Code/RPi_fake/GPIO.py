8cdc0db2644a153292e80fcda1a08a292ba8a6d3
Charles Jacobs <cjacobs@microsoft.com>
2019-09-14 16:46:21 -0700

    Updated fake GPIO

--- Code/RPi_fake/GPIO.py
+++ Code/RPi_fake/GPIO.py
@@ -2,15 +2,21 @@
 
 BCM = 0
 
+IN = 0
 OUT = 1
 
 LOW = 0
 HIGH = 1
 
+PUD_UP = 0
+PUD_DOWN = 1
+
+BOTH = 2
+
 def setmode(mode):
     pass
 
-def setup(pin, mode):
+def setup(pin, mode, *args, **kwargs):
     pass
 
 def output(pin, value):
@@ -20,4 +26,10 @@ def cleanup():
     pass
 
 def setwarnings(*args):
+    pass
+
+def add_event_detect(*args, **kwargs):
+    pass
+
+def add_event_callback(*args, **kwargs):
     pass
